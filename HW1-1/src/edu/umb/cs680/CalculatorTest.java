ca168699cc9548eb14939fe504b3ea695dddfb9e
sayaligedam <91810513+Gedam12@users.noreply.github.com>
2021-11-13 11:45:11 -0500

    Update CalculatorTest.java

--- HW1-1/src/edu/umb/cs680/CalculatorTest.java
+++ HW1-1/src/edu/umb/cs680/CalculatorTest.java
@@ -9,15 +9,15 @@ class CalculatorTest {
     @Test
     public void multiply3By4() {
         Calculator cut = new Calculator();
-        float expected = 12;
-        float actual = cut.multiply(3,4);
+        float expected = 6;
+        float actual = cut.multiply(2,3);
         assertTrue(cut instanceof Calculator);
         assertEquals(expected, actual);
     }
     @Test
     public void divide3By2(){
         Calculator cut = new Calculator();
-        float expected = 1.5f;
+        float expected = 1.5;
         float actual = cut.divide(3,2);
         assertEquals(expected, actual);
     }
@@ -25,7 +25,7 @@ class CalculatorTest {
     public void divide5By0withTryCatch(){
         Calculator cut = new Calculator();
         try{
-            cut.divide(5, 0);
+            cut.divide(4, 0);
             fail("Division by zero");
         }
         catch(IllegalArgumentException ex){
