6ce250a61d5577e3de0e3482963debdc852cda2a
sayaligedam <91810513+Gedam12@users.noreply.github.com>
2021-12-26 22:18:35 -0500

    Update ReverseOrderCar.java

--- HW13/src/edu/umb/hw13/ReverseOrderCar.java
+++ HW13/src/edu/umb/hw13/ReverseOrderCar.java
@@ -1,3 +1,37 @@
+package edu.umb.cs680.hw13;
+import java.util.ArrayList;
+import java.util.List;
+class car {
+   String name; 
+   int price; 
+   int model; 
+   public String getName() {
+      return name; 
+   } 
+   public int getPrice() { 
+      return price; 
+   } 
+   public int getModel() { 
+      return Model; 
+   } 
+   Student(String A, int a, int i){ 
+      name = A; 
+      age = a; 
+      id = i; 
+   } 
+   @Override public String toString() {     
+      return ("Car[ "+"Name:"+this.getName()+             
+              " Price: "+ this.getPrice() +                     
+              " Model: "+ this.getModel()+"]"); 
+   }
+}
+public class Example {
+   public static void main(String[] args) {
+      List<Car> carlist = new ArrayList<car>();
+      carlist.add(new car("Audi", 12000, XL)); 
+      carlist.add(new car("BMW", 22000, X3)); 
+      System.out.println("Before Sorting the car data:"); 
+
 
 Carlist.sort((car c1, car c2)->c2.getName().compareTo(c1.getName()));
 
@@ -6,3 +40,5 @@ Carlist.sort((car c1, car c2)->c2.getName().compareTo(c1.getName()));
 carlist.sort((car c1, car c2)->c2.getPrice()-c1.getPrice());
 //Lambda expression for sorting the list by car model in reverse order 
 carlist.sort((car c1, car c2)->c2.getModel()-c1.getModel());
+   }
+}
