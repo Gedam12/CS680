1050950db1d52547cdae1e886f036145488eff10
sayaligedam <91810513+Gedam12@users.noreply.github.com>
2021-12-27 00:28:39 -0500

    Update Car.java

--- HW13/src/edu/umb/hw13/Car.java
+++ HW13/src/edu/umb/hw13/Car.java
@@ -1,54 +1,73 @@
 package edu.umb.cs680.hw13;
-import java.util.ArrayList;
-import java.util.List;
-class car {
-   String name; 
-   int price; 
-   int model; 
-   public String getName() {
-      return name; 
-   } 
-   public int getPrice() { 
-      return price; 
-   } 
-   public int getModel() { 
-      return Model; 
-   } 
-   Student(String A, int a, int i){ 
-      name = A; 
-      age = a; 
-      id = i; 
-   } 
-   @Override public String toString() {     
-      return ("Car[ "+"Name:"+this.getName()+             
-              " Price: "+ this.getPrice() +                     
-              " Model: "+ this.getModel()+"]"); 
-   }
-}
-public class Example {
-   public static void main(String[] args) {
-      List<Car> carlist = new ArrayList<car>();
-      carlist.add(new car("Audi", 12000, XL)); 
-      carlist.add(new car("BMW", 22000, X3)); 
-      System.out.println("Before Sorting the car data:"); 
-  
-      carlist.forEach((c)->System.out.println(c));
 
-      System.out.println("After Sorting the car data by Model:"); 
+import java.util.*;
 
-      //Lambda expression for sorting by Model 
-      Carlist.sort((car c1, car c2)->c1.getModel()-c2.getModel()); 
-     
-      carlist.forEach((c)->System.out.println(c));         
+public class Car {
+    private int price;
+    private int year;
+    private float mileage;
+    private ArrayList<Car> cars;
+    private String name;
+    int dominationCount = 0;
 
-      car.out.println("After Sorting the car data by Name:"); 
-      //Lambda expression for sorting the list by car name       
-      carlist.sort((car c1, car c2)->c1.getName().compareTo(c2.getName())); 
-      carlist.forEach((c)->System.out.println(c));        
-      System.out.println("After Sorting the car data by Name:"); 
-     
-      //Lambda expression for sorting the list by car model
-      carlist.sort((car c1, car c2)->c1.getname()-c2.getname()); 
-      carlist.forEach((c)->System.out.println(c)); 
-   }
+
+    public Car(String name,int year,int price,float mileage) {
+
+        this.year=year;
+        this.price=price;
+        this.mileage=mileage;
+        this.name=name;
+
+    }
+    public String toString(){
+        return this.name;
+    }
+    public int getPrice(){
+        return this.price;
+    }
+    public float getMileage(){
+        return this.mileage;
+    }
+    public int getYear(){
+        return this.year;
+    }
+    public void setCars(ArrayList<Car>cars){
+        this.cars=cars;
+    }
+
+    public int getDominationCount(){
+        int count=0;
+        for(Car c:this.cars){
+            if(this.price==c.getPrice()&&this.year==c.getYear()&&this.mileage==c.getMileage()){
+                continue;
+
+            }
+            if(this.price>=c.getPrice()&&this.year<=c.getYear()&&this.mileage>=c.getMileage()){
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+    public void dominationCount(List<Car> carList, Car car) {
+
+        int price = car.getPrice();
+        int year = car.getYear();
+        float mileage = car.getMileage();
+        for (int i = 0; i < carList.size(); i++) {
+            int price1 = carList.get(i).getPrice();
+            int year1 = carList.get(i).getYear();
+            float mileage1 = carList.get(i).getMileage();
+            if (price1 >= price && year1 >= year && mileage1 >= mileage) {
+                dominationCount += 1;
+                car.setDominationCount(dominationCount);
+            }
+        }
+
+    }
+    public void setDominationCount(int dominationCount) {
+        this.dominationCount = dominationCount;
+    }
+
 }
